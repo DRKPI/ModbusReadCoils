753aa439e4a0fb8665899b1eb35279b5b910075e
David Koch <drkinvestigations@gmail.com>
2018-05-23 21:55:58 -0600

    Add project files.

---
+++ Function 01 (01hex) Read Coils/FormMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Function_01__01hex__Read_Coils
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.txtReceiveMsg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(207, 145);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(142, 40);
+            this.btnSendMsg.TabIndex = 0;
+            this.btnSendMsg.Text = "SendMsg";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SendMsg:";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(207, 30);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(718, 30);
+            this.txtSendMsg.TabIndex = 2;
+            // 
+            // txtReceiveMsg
+            // 
+            this.txtReceiveMsg.Location = new System.Drawing.Point(207, 82);
+            this.txtReceiveMsg.Name = "txtReceiveMsg";
+            this.txtReceiveMsg.Size = new System.Drawing.Size(718, 30);
+            this.txtReceiveMsg.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ReceiveMsg:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 456);
+            this.Controls.Add(this.txtReceiveMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendMsg);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Function 01 (01hex) Read Coils";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.TextBox txtReceiveMsg;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Function 01 (01hex) Read Coils/FormMain.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Function_01__01hex__Read_Coils
+{
+    public partial class FormMain : Form
+    {
+        private SerialPort sp = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte slaveAddress = 32;// Slave Address
+                byte functionCode = 03;// Function
+                ushort startAddress = 40001;// Starting Address
+                ushort numberOfPoints = 2;// Quantity of Registers
+
+
+                // Build Message(FC01)
+
+                byte[] frame = new byte[8];// total 8 bytes
+                frame[0] = slaveAddress;// Slave Address
+                frame[1] = functionCode;// Function
+                frame[2] = (byte)(startAddress >> 8);// Starting Address High
+                frame[3] = (byte)startAddress;// Starting Address Low
+                frame[4] = (byte)(numberOfPoints >> 8);// quantity of Registers High
+                frame[5] = (byte)numberOfPoints;// quantity of Registers Low
+                byte[] checkSum = CRC16(frame);// call funtion CRC Calculate
+                frame[6] = checkSum[0];// error check Low
+                frame[7] = checkSum[1];// error check High
+                sp.Write(frame, 0, frame.Length);// send frame
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                byte[] buffRec = new byte[sp.BytesToRead];
+                int numberOfBytes = sp.Read(buffRec, 0, buffRec.Length);
+
+                string sendMsg = string.Empty;
+                string receiveMsg = string.Empty;
+
+                //Send string
+                foreach(var item in frame)
+                {
+                    sendMsg += string.Format("{0:X2} ", item);
+                }
+
+                //Receive string
+                foreach (var item in buffRec)
+                {
+                    receiveMsg += string.Format("{0:X2} ", item);
+                }
+
+                txtSendMsg.Text = sendMsg;
+                txtReceiveMsg.Text = receiveMsg;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sp = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+                sp.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// CRC16
+        /// </summary>
+        /// <param name="data">byte[]</param>
+        /// <returns>byte[] of type checkSum</returns>
+        private static byte[] CRC16(byte[] data)
+        {
+            byte[] checkSum = new byte[2];
+            ushort reg_crc = 0Xffff;
+
+            for (int i = 0; i < data.Length - 2; i++)
+            {
+                reg_crc ^= data[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((reg_crc & 0x01) == 1)
+                    {
+                        reg_crc = (ushort)((reg_crc >> 1) ^ 0xA001);
+                    }
+                    else
+                    {
+                        reg_crc = (ushort)(reg_crc >> 1);
+                    }
+                }
+            }
+            checkSum[1] = (byte)((reg_crc >> 8) & 0xFF);
+            checkSum[0] = (byte)(reg_crc & 0xFF);
+
+            return checkSum;
+        }// end Function checkSum
+    }
+}
